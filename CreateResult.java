91640b566d58ef24576d2580be91187f601b6aa1
david <tonage72@gmail.com>
2021-12-08 23:28:18 -0500

    Need to fix last month of year switchover

--- CreateResult.java
+++ CreateResult.java
@@ -1,20 +1,25 @@
-import java.util.ArrayList;
-
 public class CreateResult {
 
     String[][] monthDays = {{"January","31"},{"February","28"},{"March","31"},{"April","30"},{"May","31"},{"June","30"},{"July","31"},
         {"August","31"},{"September","30"},{"October","31"},{"November","30"},{"December","31"}};
 
     protected CreateResult(int monthNumber, int dayNumber, int lessonsNumber) {
-        for(int i = 0 ; i < 12 ; i++) {
-            for(int j = 0 ; j < 2 ; j++) {
-                System.out.println(monthDays[i][j] + " ");
+        
+        monthNumber = monthNumber -1;
+
+        for(int i = 0; i < lessonsNumber; i++) {
+            System.out.println(monthDays[monthNumber][0] + " " + dayNumber + " - " + "Ch " + Integer.toString(i + 1));
+        
+            if (dayNumber == Integer.parseInt(monthDays[monthNumber][1])) {
+                if (monthNumber == 12) {
+                    monthNumber = 0;
+                } else {
+                    monthNumber = monthNumber + 1;
+                    dayNumber = 1;
+                }
+            } else {
+                dayNumber++;
             }
         }
-
-        System.out.println(Integer.toString(monthNumber));
-        System.out.println(Integer.toString(dayNumber));
-        System.out.println(Integer.toString(lessonsNumber));
-
     }
 }
